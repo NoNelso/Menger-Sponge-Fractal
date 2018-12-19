12aadafebd4a717545d3d18089738e2f053a1a89
NoNelso <jasonr4nd@gmail.com>
2018-12-19 15:37:55 -0500

    added box.js to index

--- sketch.js
+++ sketch.js
@@ -3,14 +3,15 @@ var a = 0;
 function setup() {
   createCanvas(600, 600, WEBGL);
   normalMaterial
+  var b = new Box(0, 0, 0, 200);
 }
 
 function draw() {
   background(0);
   stroke(255);
   noFill();
-  //translate(width / 2, height / 2);
+  translate(width / 2, height / 2);
   rotateX(a);
-  box(200);
+  b.show();
   a += 0.01;
 }
