f130a2797b0ae7a759d1f041bed1b0293afb0fe0
NoNelso <jasonr4nd@gmail.com>
2018-12-19 21:22:57 -0500

    fixed sum asignment so it asigns

--- box.js
+++ box.js
@@ -7,7 +7,7 @@ function Box(x, y, z, r) {
     for (var x = -1; x < 2; ++x) {
       for (var y = -1; y < 2; ++y) {
         for (var z = -1; z < 2; ++z) {
-          var newR = this.r / 3;
+          var sum = abs(x) + abs(y) + abs(z);
           var newR = this.r / 3;
           if (sum > 1) {
             var b = new Box(this.pos.x + x * newR, this.pos.y + y * newR, this.pos.z + z * newR, newR);
@@ -22,6 +22,8 @@ function Box(x, y, z, r) {
   this.show = function() {
     push();
     translate(this.pos.x, this.pos.y, this.pos.z);
+    stroke(255);
+    fill(70);
     box(this.r);
     pop();
   }
